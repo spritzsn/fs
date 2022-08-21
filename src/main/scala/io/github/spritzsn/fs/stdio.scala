680c2bb944798b65decdf2738c44d69e7f4e75c5
Edward A Maxedon Sr <edadma@gmail.com>
2022-08-21 10:30:51 -0400

    add stdio constants

--- src/main/scala/io/github/spritzsn/fs/stdio.scala
+++ src/main/scala/io/github/spritzsn/fs/stdio.scala
@@ -1,6 +1,6 @@
 package io.github.spritzsn.fs
 
-import scalanative.posix.unistd.STDOUT_FILENO
+import scalanative.posix.unistd.{STDIN_FILENO, STDOUT_FILENO, STDERR_FILENO}
 
 val stdin = new FileHandle(STDIN_FILENO)
 val stdout = new FileHandle(STDOUT_FILENO)
