af8dd0c44b66f69d4b512e7ee72c7ca0f4770916
Edward A Maxedon Sr <edadma@gmail.com>
2022-09-09 18:16:23 -0400

    test

--- src/main/scala/io/github/spritzsn/fs/Main.scala
+++ src/main/scala/io/github/spritzsn/fs/Main.scala
@@ -1,17 +1,17 @@
-package io.github.spritzsn.fs
-
-import io.github.spritzsn.libuv.*
-import io.github.spritzsn.async.loop
-
-import scala.util.{Failure, Success}
-
-@main def run(): Unit =
-  open("asdf", "wx") onComplete {
-    case Success(h) =>
-      h.write("zero\n".getBytes) onComplete {
-        case Success(h)         => h.close
-        case Failure(exception) => println(exception.getMessage)
-      }
-    case Failure(exception) => println(exception.getMessage)
-  }
-  loop.run()
+//package io.github.spritzsn.fs
+//
+//import io.github.spritzsn.libuv.*
+//import io.github.spritzsn.async.loop
+//
+//import scala.util.{Failure, Success}
+//
+//@main def run(): Unit =
+//  open("asdf", "wx") onComplete {
+//    case Success(h) =>
+//      h.write("zero\n".getBytes) onComplete {
+//        case Success(h)         => h.close
+//        case Failure(exception) => println(exception.getMessage)
+//      }
+//    case Failure(exception) => println(exception.getMessage)
+//  }
+//  loop.run()
